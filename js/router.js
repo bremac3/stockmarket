5c8b5f795f97a90f07e103b0333572b9fbd3aa6a
Breanna McIntyre <bmcinty9@uwo.ca>
2015-03-07 19:22:16 -0500

    commit

--- js/router.js
+++ js/router.js
@@ -1,5 +1,5 @@
 Stock.Router.map(function() {
-    this.resource('stockStateSummary', {path: '/market/1'},  function(){
+    this.resource('stockStateSummary', {path: '/'},  function(){
         this.resource('placeBidOrder', {path: '/placeBidOrder/:company_id'});
         this.resource('marketByOrder', {path: '/market/:company_id'});
         this.resource('placeSaleOrder', {path: '/placeSaleOrder/:company_id'});
